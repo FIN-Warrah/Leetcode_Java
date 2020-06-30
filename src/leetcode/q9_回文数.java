839c7462177591b021baaad5c0651fb0c0c205a1
FIN-Warrah <42465432+FIN-Warrah@users.noreply.github.com>
2020-06-30 19:25:05 +0800

    q9

---
+++ src/leetcode/q9_回文数.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class q9_回文数 {
+
+    class Solution {
+        public boolean isPalindrome(int x) {
+            // 排除特殊情况
+            if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+            // another表示反转后的后一半
+            int another = 0;
+            // 当前一半小于或等于反转后的后一半时，循环结束
+            while (! (x <= another)) {
+                another = another * 10 + x % 10;
+                x /= 10;
+            }
+            // x的长度为偶数或x的长度为奇数
+            return x == another || x == another / 10;
+        }
+    }
+}
