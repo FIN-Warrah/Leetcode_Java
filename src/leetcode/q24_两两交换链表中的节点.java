0038c6d0a488712417619bf18d2e9e6c07201887
FIN-Warrah <42465432+FIN-Warrah@users.noreply.github.com>
2020-07-05 09:25:22 +0800

    q24

---
+++ src/leetcode/q24_两两交换链表中的节点.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+public class q24_两两交换链表中的节点 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    class Solution {
+        public ListNode swapPairs(ListNode head) {
+            // 用于返回结果
+            ListNode dummy = new ListNode(-1);
+            dummy.next = head;
+
+            ListNode curr = head;
+            ListNode preNode = dummy;
+            while (curr != null && curr.next != null) {
+                ListNode node1 = curr;
+                ListNode node2 = curr.next;
+                // 交换
+                preNode.next = node2;
+                node1.next = node2.next;
+                node2.next = node1;
+
+                preNode = node1;
+                curr = node1.next;
+            }
+            return dummy.next;
+        }
+    }
+
+    class Solution2 {
+        public ListNode swapPairs(ListNode head) {
+            // 结束条件
+            if (head == null || head.next == null)
+                return head;
+
+            ListNode node1 = head;
+            ListNode node2 = head.next;
+
+            // 交换
+            node1.next  = swapPairs(node2.next);
+            node2.next = node1;
+
+            return node2;
+        }
+    }
+}
