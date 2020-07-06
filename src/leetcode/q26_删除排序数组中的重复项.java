53c5464938241681d6878d5aaa689737eb5b543d
FIN-Warrah <42465432+FIN-Warrah@users.noreply.github.com>
2020-07-06 15:54:52 +0800

    q26

---
+++ src/leetcode/q26_删除排序数组中的重复项.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class q26_删除排序数组中的重复项 {
+
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            if (nums == null || nums.length == 0) return 0;
+            int i = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] != nums[j]) {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+            return i + 1;
+        }
+    }
+}
